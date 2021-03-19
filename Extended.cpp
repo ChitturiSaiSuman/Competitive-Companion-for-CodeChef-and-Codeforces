634817d4d2b85911e82bbe5ea2e9ba97be065331
ChitturiSaiSuman <saisumanchitturi@gmail.com>
2021-03-19 10:11:24 +0530

    Resolved many bugs in Default.cpp

--- Extended.cpp
+++ Extended.cpp
@@ -19,21 +19,30 @@ SPOJ: Sai Suman Chitturi @out_of_bound
 #include <bits/stdc++.h>
 using namespace std;
 
-#define nl							"\n"
-#define sp							" "
+#define newline 				    cout << "\n"
+#define space					    cout << " "
+#define endl					    cout << "\n"
 #define iter(x, a, b) 			    for(int x = a; x <= b; x++)
 #define range(start, stop, step)    for(int it = start; it < stop; it += step)
 #define FOR(x, N) 				    for(int x = 0; x < N; x++)
 #define For(x, N) 				    for(int x = 0; x < N; x++)
-#define caseprint                   cout << "Case #" << (test + 1) << ": "
-#define inverse(a, p) 			    power(a, p - 2, p)
+#define caseprint                   cout << "Case #" << d << ": "
+#define inverse(a, p) 			    power(a, p-2, p)
+#define scan(a) 				    cin >> a
+#define print(a) 				    cout << a
+#define println(a)                  cout << a << "\n";
 #define getName(var)			    #var
 #define debug(var)                  cerr << getName(var) << " = " << var << "\n"
 #define abort					    assert(false)
 #define read(arr, nax)              FOR(IT, nax) {cin >> arr[IT];}
 #define write(arr, nax)             FOR(IT, nax) {cout << arr[IT] << " ";}
 #define fill(arr,nax,value)		    FOR(IT,nax) {arr[IT] = value;}
+#define sort123(arr,nax)		    qsort(arr,nax,sizeof(int),ascending)
+#define sort321(arr,nax)		    qsort(arr,nax,sizeof(int),descending)
 #define reverse(arr,nax)		    FOR(x,nax/2) {arr[x]=arr[nax-x-1];}
+#define newInt(nax)				    (int*)malloc(sizeof(int)*nax)
+#define newLong(nax)			    (ll *)malloc(sizeof(ll)*nax)
+#define copy(from,to,nax)           FOR(IT,nax) {to[IT] = from[IT];}
 
 typedef unsigned long long int ull;
 typedef long long int ll;
@@ -62,67 +71,78 @@ static inline int sumOfDigits(ll n) 			{return n>0?n%10+sumOfDigits(n/10):0;}
 static inline int numberOfDigits(ll n)			{return n>0?1+numberOfDigits(n/10):0;}
 
 ll power(ll x, ll y, ll p) {
-	ll result = 1;
-	for(; y > 0; y >>= 1, x = mul(x, x, p)) {
+	ll result=1;
+	for(;y>0;y>>=1,x=mul(x,x,p)) {
 		if(y&1)
-			result = mul(result, x, p);
+			result = mul(result,x,p);
 	}
 	return result;
 }
 
 bool isPrime(ll n) {
-	if(n == 0 || n == 1)
+	if(n==0 or n==1)
 		return false;
-	else if(n == 2 || n == 3)
+	else if(n==2 or n==3)
 		return true;
-	else if(n % 2 == 0 || n % 3 == 0)
+	else if(n%2==0 or n%3==0)
 		return false;
-	for(int i = 5; i <= sqrt(n); i += 6)
-		if(n % i == 0 || n % (i + 2)== 0)
+	for(int i=5;i<=sqrt(n);i+=6)
+		if(n%i==0 or n%(i+2)==0)
 			return false;
 	return true;
 }
 
-void preCompute() {
 
-    return;
-}
-
-void solve() {
-
-
-	return;
-}
-
-int main() {
-
-    ios_base::sync_with_stdio(false), cin.tie(NULL);
-
-    int t = 0
-	;
-	
-	if(!t) cin >> t;
-	preCompute();
-	For(test, t) {
-		// caseprint;
-		solve();
+class Fraction {
+	public:
+	ll num = 0, den = 1;
+	ll gcd(ll a, ll b) {for(ll rem;b>0;rem=a%b,a=b,b=rem); return a;}
+	ll lcm(ll a, ll b) {return (a * b) / gcd(a, b);}
+	Fraction() {
+		num = 0;
+		den = 1;
+	};
+	Fraction(ll n) {
+		num = n;
+		den = 1;
+	};
+	Fraction(ll n, ll d) {
+		if(d == 0) {
+			throw invalid_argument("Non-Zero denominator is expected");
+		}
+		num = n;
+		den = d;
+		ll g = gcd(num, den);
+		num /= g;
+		den /= g;
+	};
+	Fraction operator + (Fraction const &frac) {
+		ll l = lcm(den, frac.den);
+		ll a = num * (l / den);
+		ll b = frac.num * (l / frac.den);
+		return Fraction(a+b, l);
 	}
-	return 0;
-}
+	Fraction operator - (Fraction const &frac) {
+		ll l = lcm(den, frac.den);
+		ll a = num * (l / den);
+		ll b = frac.num * (l / frac.den);
+		return Fraction(a - b, l);
+	}
+	Fraction operator * (Fraction const &frac) {
+		return Fraction(num * frac.num, den * frac.den);
+	}
+	Fraction operator / (Fraction const &frac) {
+		return Fraction(num * frac.den, den * frac.num);
+	}
+	operator string() const {
+		if(den == 1)
+			return to_string(num);
+		return to_string(num) + "/" + to_string(den);
+	}
+};
 
-vector<bool> sieve(int nax) {
-    vector<bool> prime(nax, false);
-    prime[2] = true;
-    for(int i = 3; i < nax; i += 2) {
-        prime[i] = true;
-    }
-
-    for(int i = 3; i * i < nax; i += 2) {
-        if(prime[i]) {
-            for(int j = i * i; j < nax; j += i) {
-                prime[j] = false;
-            }
-        }
-    }
-    return prime;
+ostream& operator << (ostream& out, const Fraction &f) {
+	if(f.den == 1)
+		return out << to_string(f.num);
+	return out << to_string(f.num) + "/" + to_string(f.den);
 }
