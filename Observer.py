eca12b24471a1d8094905eba872ea83a87126c98
ChitturiSaiSuman <saisumanchitturi@gmail.com>
2022-01-07 09:49:25 +0530

    time.sleep() is no longer used to wait for the page to load

--- Observer.py
+++ Observer.py
@@ -11,24 +11,24 @@
 # 7. Repeat steps 3-6
 
 from selenium import webdriver
-
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-# Use the above two imports instead of sleep() for better performance
-
 from selenium.webdriver.common.by import By
 
+from time import sleep
 from gtts import gTTS
 from os import system
-from time import sleep
 
 # Path to Templates, modify this based on your system
 path_to_templates = '/home/suman/Competitive-Companion-for-Codechef'
 
 def speak(text: str) -> None:
+
     object = gTTS(text = text, lang = 'en', slow = False)
     destination_file = path_to_templates + '/Voice.mp3'
     object.save(destination_file)
+
+    print(text, flush = True)
     system('mpg123 ' + destination_file)
     system('rm ' + destination_file)
 
@@ -43,15 +43,20 @@ def check_status(url: str) -> None:
 
     # Open the Profile page of the User
     driver.get(url)
-    sleep(4)
 
     # Get the last submission of the User
-    element = driver.find_element(By.XPATH, '//tbody/tr[1]/td[3]/span[1]/img[1]')
+    element = WebDriverWait(driver, 100).until(EC.presence_of_all_elements_located((By.XPATH, "//tbody/tr[1]/td[3]/span[1]/img[1]")))[0]
+
+    # element = driver.find_element(By.XPATH, '')
 
     # Get the status of the last submission
     src = element.get_attribute('src')
     if 'tick' in src:
         speak("Your submission has been accepted")
+    elif 'loader' in src:
+        sleep(5)
+        print("Your submission is being processed", flush = True)
+        check_status(url)
     else:
         speak("Your submission was not accepted")
 
@@ -68,22 +73,24 @@ def start(url: str) -> None:
 
     # Open the Profile page of the User
     driver.get(url)
-    sleep(4)
 
     # Get the last submission of the User
-    element = driver.find_element(By.XPATH, relative_xpath)
+    # Waits until the element loads on the page
+    element = WebDriverWait(driver, 100).until(EC.presence_of_all_elements_located((By.XPATH, relative_xpath)))[0]
+
     last_submission = element.get_attribute('href')
     last_submission_id = last_submission.split('/')[-1]
 
     while True:
         driver.get(url)
-        sleep(5)
-        element = driver.find_element(By.XPATH, relative_xpath)
+        element = WebDriverWait(driver, 100).until(EC.presence_of_all_elements_located((By.XPATH, relative_xpath)))[0]
+
         current_submission = element.get_attribute('href')
 
         # If the last submission has changed, check the status of the submission
         current_submission_id = current_submission.split('/')[-1]
         if current_submission_id != last_submission_id:
+            print("New Submission Detected. Submission id: " + current_submission_id, flush = True)
             check_status(url)
 
         last_submission_id = current_submission_id
