be8ab4f08fb8d7f89381a0d0c1a9132fed4f7c0b
ChitturiSaiSuman <saisumanchitturi@gmail.com>
2022-02-27 20:44:00 +0530

    Rectified some bugs

--- DEBUG.cpp
+++ DEBUG.cpp
@@ -3,6 +3,18 @@
 
 using namespace std;
 
+void debug(const char ch) {
+    cerr << ch << '\n';
+}
+
+void debug(const int N) {
+    cerr << N << '\n';
+}
+
+void debug(const long int N) {
+    cerr << N << '\n';
+}
+
 void debug(const long long N) {
     cerr << N << '\n';
 }
@@ -15,11 +27,11 @@ void debug(const float N) {
     cerr << N << '\n';
 }
 
-void debug(const long double N) {
+void debug(const double N) {
     cerr << N << '\n';
 }
 
-void debug(const double N) {
+void debug(const long double N) {
     cerr << N << '\n';
 }
 
@@ -77,13 +89,13 @@ void debug(const vector<pair<int, int>>& arr) {
         cerr << "[]\n";
     }
     else if(N == 1) {
-        cerr << "[" << arr[0].first << ", " << arr[0].second << "]\n";
+        cerr << "[(" << arr[0].first << ", " << arr[0].second << ")]\n";
     }
     else {
-        cerr << "[" << arr[0].first << ", " << arr[0].second;
+        cerr << "[(" << arr[0].first << ", " << arr[0].second;
         for(int i = 1; i < N; i++) {
-            cerr << ", " << arr[i].first << ", " << arr[i].second;
+            cerr << "), (" << arr[i].first << ", " << arr[i].second;
         }
-        cerr << "]\n";
+        cerr << ")]\n";
     }
 }
